daa93ef0e448e6e097f19eab9412607372de8292
a66668866 <a66668866@gmail.com>
2023-02-23 16:33:03 +0800

    20230223

--- jquary/jQuery/10_new.js
+++ jquary/jQuery/10_new.js
@@ -1,3 +1,17 @@
 $(function () {
-    
+    // 偵測目前div的font-size
+    // let currentSize = $('div').css('fontSize')
+    // alert(currentSize)
+
+    $('#shrink').click(function(){})
+    $('#enlarge').click(function(){
+
+    })
+    function changeSize(){
+
+    }
+
 });
+
+函數三步驟: 宣告,定義,呼叫
+JS的宣告核定義寫在一起
