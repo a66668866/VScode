26e52e7f84a342bda3b7db05808cd6a2aa58e5a3
a66668866 <a66668866@gmail.com>
2023-02-24 16:32:05 +0800

    no message

--- jquary/jQuery/10_new.js
+++ jquary/jQuery/10_new.js
@@ -1,17 +1,51 @@
 $(function () {
-    // 偵測目前div的font-size
-    // let currentSize = $('div').css('fontSize')
+    // 偵測目前 div 的 font-size
+    // let currentSize = parseInt($('div').css('fontSize'))
     // alert(currentSize)
 
-    $('#shrink').click(function(){})
-    $('#enlarge').click(function(){
-
+    $('#shrink').click(function(){
+        changeSize('small')
     })
-    function changeSize(){
-
+    $('#enlarge').click(function(){
+        changeSize('big')
+    })
+    function changeSize(size){   // 宣告 + 定義
+        let currentSize = parseInt($('div').css('fontSize'))
+        
+        if(size == 'small'){
+            newSize = currentSize - 2
+        }else if(size == 'big'){
+            newSize = currentSize + 2
+        }
+        
+        $('div').css('fontSize',newSize)
     }
-
 });
 
-函數三步驟: 宣告,定義,呼叫
-JS的宣告核定義寫在一起
+// 函數三部曲: 宣告、定義和呼叫
+// JS 的宣告和定義寫在一起
+/*=====
+1 不傳參數，沒有傳回值
+function temp(){  // 宣告、定義
+
+}
+呼叫: temp()
+
+2 有傳參數，沒有傳回值
+function temp(num){
+
+}
+呼叫: temp(5)
+
+3 不傳參數，有傳回值
+function temp(){
+    return 123
+}
+呼叫: let ans = temp()
+
+4 有傳參數，有傳回值
+function temp(num){
+    return num * 123
+}
+呼叫: let ans = temp(5)
+=====*/
